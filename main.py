3fd8ed36ac5b6355e05863685435d485b92d561a
nuriacerezo <nuria.cerezo@estudiantat.upc.edu>
2022-05-18 15:07:35 +0200

    Diseño

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow,QApplication,QWidget
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#Cargar formulario *.ui
+form_class = uic.loadUiType("diseno.ui")[0]
+
+#crear clase MyWindowClass
+class MyWindowClass(QWidget, form_class):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self,parent)
+        self.setupUi(self)
+        self.pantallas.setCurrentIndex(1)# activamos la pantalla 1
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hola Joan, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QApplication(sys.argv)
+    MyWindow = MyWindowClass(None)
+    MyWindow.show()
+    app.exec_()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
