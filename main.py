4d95a3ae66401e6d925950f3e310d2cad7f0fa1f
= <=>
2022-06-28 17:30:20 +0200

    Excel, canvis main i base de datos, funciona

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QMessageBox
 
 from Clase_Serie import *
-from Classe_Pelicula import *
+from Classe_Pelicula import  *
+from Usuario import *
 
 #include "comboboxdialog.h"
 #include "ui_comboboxdialog.h"
 
 # Cargar formulario *.ui
-form_class = uic.loadUiType("diseno.ui")[0]
+form_class = uic.loadUiType('diseno.ui')[0]
 
 
 # crear clase MyWindowClass
@@ -20,7 +21,7 @@ class MyWindowClass(QMainWindow, form_class):
         self.pantallas.setCurrentIndex(0)  # activamos la pantalla 1
 
     def btpulsado(self):
-        self.pantallas.setCurrentIndex(1)
+        self.pantallas.setCurrentIndex(2)
         nombre = self.sender().objectName()
         pelicula1 = Pelicula(nombre)
         titulo_pelicula = str(pelicula1.getTitulo())
@@ -31,7 +32,7 @@ class MyWindowClass(QMainWindow, form_class):
         self.genero.setText(genero_pelicula)
         self.duracion.setText(duracion_pelicula)
         self.visto.setText(visto_pelicula)
-
+    
     def btserie(self):
         self.pantallas.setCurrentIndex(2)
         nombre = self.sender().objectName()
@@ -41,8 +42,51 @@ class MyWindowClass(QMainWindow, form_class):
 
 
 
+
     def volver(self):
-        self.pantallas.setCurrentIndex(0)
+        self.pantallas.setCurrentIndex(1)
+
+    def bt_iniciarsesion(self):
+        nickname = self.lineEdit.text()
+        contrasena = self.lineEdit_2.text()
+        Usuario1 = Usuario(nickname, contrasena)
+        if (Usuario1.Iniciar_Sesion()==True):
+
+            self.lineEdit.clear()
+            self.lineEdit_2.clear()
+            self.pantallas.setCurrentIndex(1)
+
+        else:
+            self.label_2.setText('Usuario incorrecto')
+            self.lineEdit.clear()
+            self.lineEdit_2.clear()
+
+    def bt_crearsesion(self):
+        nickname = self.lineEdit.text()
+        contrasena = self.lineEdit_2.text()
+        Usuario1 = Usuario(nickname, contrasena)
+        if Usuario1.Comprobar_User()== True:
+            pregunta = QMessageBox()
+            pregunta.setWindowTitle('Crear Usuario')
+            pregunta.setText('Seguro que quieres crear el usuario?')
+            pregunta.setIcon(QMessageBox.Question)
+            pregunta.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+            resultado = pregunta.exec_()
+            print(resultado)
+            if (resultado == 16384):
+                Usuario1.Crear_Sesion()
+                self.lineEdit.clear()
+                self.lineEdit_2.clear()
+                self.label_2.setText('Usuario creado')
+
+            else:
+                self.lineEdit.clear()
+                self.lineEdit_2.clear()
+                self.label_2.setText('Usuario no creado')
+        else:
+            self.lineEdit.clear()
+            self.lineEdit_2.clear()
+            self.label_2.setText('Usuario existente')
 
 
 if __name__ == '__main__':
@@ -50,4 +94,3 @@ if __name__ == '__main__':
     MyWindow = MyWindowClass(None)
     MyWindow.show()
     app.exec_()
-
